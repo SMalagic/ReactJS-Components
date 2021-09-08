4f94859ec92788a576714290234095805524c277
SMalagic <serkanmalagic@hotmail.com>
2021-09-08 11:38:48 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  const expenses = [
+    { title: "Car insurance", amount: 200.56, date: new Date(2021, 12, 12) },
+    { title: "Car insurance", amount: 200.56, date: new Date(2021, 12, 12) },
+    { title: "Car insurance", amount: 200.56, date: new Date(2021, 12, 12) },
+    { title: "Car insurance", amount: 200.56, date: new Date(2021, 12, 12) },
+  ];
+
+  return (
+    <div>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;

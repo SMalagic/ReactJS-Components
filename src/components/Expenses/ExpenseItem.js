900a1438e81e698e82e47f8010f3d9ee037caf83
SMalagic <serkanmalagic@hotmail.com>
2021-09-10 11:32:31 +0300

    Expense update
    - Folder structure has changed
    - New component has added

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState("Serkan");
+
+  const clickHandler = () => {
+    setTitle("sd");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Click Me</button>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
